dec5e6f82ff3f2f0f90c2c9a1e9558c7a62edcf6
Thomas F. Hagelien <thomas.f.hagelien@sintef.no>
2026-02-09 15:39:47 +0100

    Fixing CI/CD issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const repo = process.env.GITHUB_REPOSITORY?.split('/')[1];
+    const isUserPage = repo?.endsWith('.github.io');
     return {
+      base: mode === 'production' ? (repo && !isUserPage ? `/${repo}/` : '/') : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
